8e271194b3e7b626f540e6ea91900714e0745e2a
Pramod Adithya Muntimadugu <pramodadithya@gmail.com>
2023-09-27 21:59:14 -0500

    Add functionality to focus search input when pressing the enter key and other appropriate behavior as per requirements

--- use-popcorn/src/components/search-box/SearchBox.jsx
+++ use-popcorn/src/components/search-box/SearchBox.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./SearchBox.css";
 
 export default function SearchBox({ searchQuery, setSearchQuery }) {
+  const searchInput = useRef();
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === searchInput.current) return;
+      if (e.code === "Enter") {
+        searchInput.current.focus();
+        setSearchQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, []);
+
   return (
     <input
       className="search"
       placeholder="Search for a movie..."
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
+      ref={searchInput}
     />
   );
 }
